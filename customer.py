34b16b9db29b81284e7784807fa9b205a4eb98fa
EpicLulz1509 <duttaarunopal@gmail.com>
2024-06-28 15:48:00 +0530

    Update customer.py

--- customer.py
+++ customer.py
@@ -18,8 +18,8 @@ def createCustomerTable(path):
     
     cust_schema = customer_schema() 
 
-    location_target_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).load("tgtdata\location.csv")
-    customer_data_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).schema(cust_schema).load("srcdata\CustomerData\CustomerData.csv")
+    location_target_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).load("tgtdata/location.csv")
+    customer_data_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).schema(cust_schema).load("srcdata/CustomerData/CustomerData.csv")
 
     customer_target_df = customer_data_df.join(location_target_df,( F.col("PostalCode") == F.col("LocationZip") ) & (F.col("City") == F.col("LocationCity")))
     customer_target_df = customer_target_df.select(F.col("CustomerId"), F.col("CustomerKey"), F.col("LocationId")).orderBy(F.col("CustomerId"))
