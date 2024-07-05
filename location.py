7235f3ba2d0e9e8ed6e80cf539f38f97044c7c1d
EpicLulz1509 <duttaarunopal@gmail.com>
2024-07-05 11:48:15 +0530

    Update location.py

--- location.py
+++ location.py
@@ -19,9 +19,9 @@ def createLocationTable(path):
     cust_schema = customer_schema()
     sales_terr_schema = sales_territory_schema()
 
-    customer_data_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).schema(cust_schema).load("srcdata\\CustomerData\\CustomerData.csv")
-    sales_territory_df = spark.read.format(file_type).option("sep", delimiter).schema(sales_terr_schema).load("srcdata\SalesTerritory\SalesTerritory.csv")
-    country_target_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).load("tgtdata\country.csv")
+    customer_data_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).schema(cust_schema).load("srcdata/CustomerData/CustomerData.csv")
+    sales_territory_df = spark.read.format(file_type).option("sep", delimiter).schema(sales_terr_schema).load("srcdata/SalesTerritory/SalesTerritory.csv")
+    country_target_df = spark.read.format(file_type).option("sep", delimiter).option("header", first_row_is_header).load("tgtdata/country.csv")
 
     location_target_df = customer_data_df.join(sales_territory_df, F.col("CountryRegion") == F.col("Country")).select(F.col("City"), F.col("StateProvince"), F.col("Country"), F.col("PostalCode"))
     location_target_df = location_target_df.distinct()
